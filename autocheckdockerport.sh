8366ce15fd2bbae9a67a8a77881fec64d3b4e1ff
whunt1 <35031982+whunt1@users.noreply.github.com>
2019-05-14 21:48:37 +0800

    Update autocheckdockerport.sh

--- autocheckdockerport.sh
+++ autocheckdockerport.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 #description:自动映射端口脚本
-RULER=$(iptables -t nat -nvL --line-number | grep "172.17.0.2:443" | awk '{print $1}')
+RULER=$(/usr/sbin/iptables -t nat -nvL --line-number | grep "172.17.0.2:443" | awk '{print $1}')
 echo ${RULER}
 if [[ -z $RULER ]]; then
  echo "rebuild"
- iptables -t filter -A DOCKER -d 172.17.0.2/32 ! -i docker0 -o docker0 -p tcp -m tcp --dport 443 -j ACCEPT
- iptables -t nat -A POSTROUTING -s 172.17.0.2/32 -d 172.17.0.2/32 -p tcp -m tcp --dport 443 -j MASQUERADE
- iptables -t nat -A DOCKER ! -i docker0 -p tcp -m tcp --dport 443 -j DNAT --to-destination 172.17.0.2:443
+ /usr/sbin/iptables -t filter -A DOCKER -d 172.17.0.2/32 ! -i docker0 -o docker0 -p tcp -m tcp --dport 443 -j ACCEPT
+ /usr/sbin/iptables -t nat -A POSTROUTING -s 172.17.0.2/32 -d 172.17.0.2/32 -p tcp -m tcp --dport 443 -j MASQUERADE
+ /usr/sbin/iptables -t nat -A DOCKER ! -i docker0 -p tcp -m tcp --dport 443 -j DNAT --to-destination 172.17.0.2:443
 fi
