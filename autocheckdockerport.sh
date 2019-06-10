8171baa51ca271d3b5a7ee1c37ae445774184dd3
whunt1 <35031982+whunt1@users.noreply.github.com>
2019-06-11 01:31:57 +0800

    Add files via upload

--- autocheckdockerport.sh
+++ autocheckdockerport.sh
@@ -1,10 +1,17 @@
 #!/bin/bash
-#description:自动映射端口脚本
-RULER=$(/usr/sbin/iptables -t nat -nvL --line-number | grep "172.17.0.2:443" | awk '{print $1}')
-echo ${RULER}
-if [[ -z $RULER ]]; then
- echo "rebuild"
+# manual run
+# /usr/sbin/iptables -t nat -A POSTROUTING -s 172.17.0.2/32 -d 172.17.0.2/32 -p tcp -m tcp --dport 443 -j MASQUERADE
+# /usr/sbin/iptables -t nat -A POSTROUTING -s 172.17.0.2/32 -d 172.17.0.2/32 -p udp -m udp --dport 443 -j MASQUERADE
+RULER1=$(/usr/sbin/iptables -t filter -nvL DOCKER --line-number | grep "dpt:443" | awk '{print $1}')
+build_iptables(){
+ /usr/sbin/iptables -t filter -A FORWARD -d 172.17.0.0/24 -i docker0 -j ACCEPT
  /usr/sbin/iptables -t filter -A DOCKER -d 172.17.0.2/32 ! -i docker0 -o docker0 -p tcp -m tcp --dport 443 -j ACCEPT
- /usr/sbin/iptables -t nat -A POSTROUTING -s 172.17.0.2/32 -d 172.17.0.2/32 -p tcp -m tcp --dport 443 -j MASQUERADE
  /usr/sbin/iptables -t nat -A DOCKER ! -i docker0 -p tcp -m tcp --dport 443 -j DNAT --to-destination 172.17.0.2:443
-fi
+ /usr/sbin/iptables -t filter -A DOCKER -d 172.17.0.2/32 ! -i docker0 -o docker0 -p udp -m udp --dport 443 -j ACCEPT
+ /usr/sbin/iptables -t nat -A DOCKER ! -i docker0 -p udp -m udp --dport 443 -j DNAT --to-destination 172.17.0.2:443
+}
+echo ${RULER1}
+if [[ -z $RULER1 ]]; then
+ echo "rebuild"
+ build_iptables
+fi 
